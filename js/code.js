bd13bd97f565076a2c7157ecdb1dcc4cac5ada5d
Max Boguslavskiy <max.bogus@gmail.com>
2017-01-17 01:52:28 +0300

    * corrected template; * corrected data; * reworked map object to use template.

--- js/code.js
+++ js/code.js
@@ -3,83 +3,80 @@
 // coord - latitude and longitude of object
 // content - info shown in info balloon in marker
 // icon - icon for custom icor
-var initialMapObjects = [
+let initialMapObjects = [
     {
         "name": "Banki.ru",
         "coordinates": {lat: 55.706770, lng: 37.625883},
-        "content": '<div>' +
-        '<h1 class="firstHeading">Banki.ru</h1>' +
-        '<div id="bodyContent">' +
-        '<p><b>Head of Quality Assurance:</b></p><ul><li>Responsible for hiring and managing of two groups of testers: local (5 people) and two remote teams.</li>' +
-        '<li>Created department from ‘scratch’ in two years and developed processes to support department goals.</li>' +
-        '<li>Realized and implemented a semi-automatic solution that enables the Smoke-testing of more than 900 pages of our website in 5 minutes;</li>' +
-        '<li>Reduced software development time in the final stages by an average of 40-50% (1 month to 2 weeks).</li></ul>' +
-        '</div>' +
-        '</div>',
         "icon": 'img/banki-ru-logo-small.png',
         "visible": true,
         "data": {
             "title": "Banki.ru",
             "subtitle": "Head of Quality Assurance:",
-            "items": ["Responsible for hiring and managing of two groups of testers: local (5 people) and two remote teams.",
+            "items": [
+                "Responsible for hiring and managing of two groups of testers: local (5 people) and two remote teams.",
                 "Created department from ‘scratch’ in two years and developed processes to support department goals.",
                 "Realized and implemented a semi-automatic solution that enables the Smoke-testing of more than 900 pages of our website in 5 minutes.",
-                "Reduced software development time in the final stages by an average of 40-50% (1 month to 2 weeks)."]
+                "Reduced software development time in the final stages by an average of 40-50% (1 month to 2 weeks)."
+            ]
         }
     },
     {
         "name": "Luxoft",
         "coordinates": {lat: 55.815230, lng: 37.573992},
-        "content": '<div>' +
-        '<h1 class="firstHeading">Luxoft</h1>' +
-        '<div id="bodyContent">' +
-        '<p><b>Test Designer:</b></p><ul><li>Was responsible for test design and manual testing of WIRS system;</li>' +
-        '<li>Developed 6 test suites for 6 modules of WIRS system.</li></ul>' +
-        '</div>' +
-        '</div>',
         "icon": 'img/luxoft-small.png',
-        "visible": true
+        "visible": true,
+        "data": {
+            "title": "Luxoft",
+            "subtitle": "Test Designer:",
+            "items": [
+                "Was responsible for test design and manual testing of WIRS system.",
+                "Developed 6 test suites for 6 modules of WIRS system."
+            ]
+        }
     },
     {
         "name": "Acronis",
         "coordinates": {lat: 55.874736, lng: 37.588088},
-        "content": '<div>' +
-        '<h1 class="firstHeading">Acronis</h1>' +
-        '<div id="bodyContent">' +
-        '<p><b>Quality Assurance Engineer:</b></p><ul><li>Was responsible for test design, requirements analysis and manual testing (regression, stress, volume, script-based and exploratory);</li>' +
-        '<li>Created 1500 TCs for manual and auto testing;</li>' +
-        '<li>Found and submitted ~6700 defects.</li></ul>' +
-        '</div>' +
-        '</div>',
         "icon": 'img/Acronis_logo-small.png',
-        "visible": true
+        "visible": true,
+        "data": {
+            "title": "Acronis",
+            "subtitle": "Quality Assurance Engineer:",
+            "items": [
+                "Was responsible for test design, requirements analysis and manual testing (regression, stress, volume, script-based and exploratory).",
+                "Created 1500 TCs for manual and auto testing.",
+                "Found and submitted ~6700 defects."
+            ]
+        }
     },
     {
         "name": "Innova",
         "coordinates": {lat: 55.747078, lng: 37.653688},
-        "content": '<div>' +
-        '<h1 class="firstHeading">Innova</h1>' +
-        '<div id="bodyContent">' +
-        '<p><b>Test manager:</b></p><ul><li>Was responsible for hiring and managing of one local team (4 people).</li>' +
-        '<li>Performed functional and exploratory testing.</li>' +
-        '<li>Successful release with low defects (20 bugs) of a new version of 4game platform in Europe and Russia.</li></ul>' +
-        '</div>' +
-        '</div>',
         "icon": 'img/4game-small.png',
-        "visible": true
+        "visible": true,
+        "data": {
+            "title": "Innova",
+            "subtitle": "Test manager:",
+            "items": [
+                "Was responsible for hiring and managing of one local team (4 people).",
+                "Performed functional and exploratory testing.",
+                "Successful release with low defects (20 bugs) of a new version of 4game platform in Europe and Russia."
+            ]
+        }
     },
     {
         "name": "Superscape",
         "coordinates": {lat: 55.724779, lng: 37.642375},
-        "content": '<div>' +
-        '<h1 class="firstHeading">Superscape</h1>' +
-        '<div id="bodyContent">' +
-        '<p><b>Tester:</b></p><ul><li>* Was responsible for manual testing;</li>' +
-        '<li>Found ~1200 defects were found and submitted personally (35% of defects could cause hardware defects in the cell phone or major data loss).</li></ul>' +
-        '</div>' +
-        '</div>',
         "icon": 'img/superscape-small.png',
-        "visible": true
+        "visible": true,
+        "data": {
+            "title": "Superscape",
+            "subtitle": "Tester:",
+            "items": [
+                "Was responsible for manual testing.",
+                "Found ~1200 defects were found and submitted personally (35% of defects could cause hardware defects in the cell phone or major data loss)."
+            ]
+        }
     }
 ];
 
@@ -95,17 +92,17 @@ function initMap() {
 ko.bindingHandlers.googleMap = {
     // init map, markers and default functionality
     init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-        var mapOptions = {
+        let mapOptions = {
             center: new google.maps.LatLng(
                 55.752198, 37.617499),
             zoom: 9
         };
-        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        let map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-        var infowindow = new google.maps.InfoWindow();
+        let infowindow = new google.maps.InfoWindow();
 
         initialMapObjects.forEach(function (mapItem) {
-            var image = {
+            let image = {
                 url: mapItem.icon,
                 // This marker is 32 pixels wide by 32 pixels high.
                 size: new google.maps.Size(32, 32),
@@ -115,11 +112,11 @@ ko.bindingHandlers.googleMap = {
                 anchor: new google.maps.Point(0, 32)
             };
 
-            var latLng = new google.maps.LatLng(
+            let latLng = new google.maps.LatLng(
                 mapItem.coordinates.lat,
                 mapItem.coordinates.lng);
 
-            var marker = new google.maps.Marker({
+            let marker = new google.maps.Marker({
                 position: latLng,
                 map: map,
                 icon: image,
@@ -146,7 +143,11 @@ ko.bindingHandlers.googleMap = {
             marker.openInfoWindow = function () {
                 marker.buildAdditionalInfo();
                 marker.toggleBounce();
-                infowindow.setContent(mapItem.content);
+                let source = $("#template").html();
+                let template = Handlebars.compile(source);
+                let html = template(mapItem.data);
+
+                infowindow.setContent(html);
                 infowindow.open(map, this);
             };
 
@@ -160,13 +161,13 @@ ko.bindingHandlers.googleMap = {
     }
 };
 
-var MapObject = function (data) {
+let MapObject = function (data) {
     this.name = ko.observable(data.name);
     this.visible = ko.observable(data.visible);
 };
 
-var ViewModel = function () {
-    var self = this;
+let ViewModel = function () {
+    let self = this;
 
     this.mapObjectList = ko.observableArray([]);
 
@@ -195,13 +196,13 @@ var ViewModel = function () {
             async: true,
             type: 'GET',
             success: function (data) {
-                var temp = [];
-                var articleList = data[1];
-                var urlList = data[3];
-                var len = articleList.length;
-                for (var i = 0; i < len; i++) {
-                    var wikiEntry = articleList[i];
-                    var url = urlList[i];
+                let temp = [];
+                let articleList = data[1];
+                let urlList = data[3];
+                let len = articleList.length;
+                for (let i = 0; i < len; i++) {
+                    let wikiEntry = articleList[i];
+                    let url = urlList[i];
                     temp.push({"url": url, "wikientry": wikiEntry});
                 }
                 self.additionalInfo(temp);
@@ -225,7 +226,7 @@ var ViewModel = function () {
 
     // bounce matching marker and open infowindow
     this.toggleBounce = function () {
-        var name = this.name().toLowerCase();
+        let name = this.name().toLowerCase();
         self.markerList().forEach(function (marker) {
             if (marker.title.toLowerCase().includes(name)) {
                 marker.openInfoWindow();
@@ -238,18 +239,18 @@ var ViewModel = function () {
     // we are creating a list of matched markers
     // if no objects are matching we are showing error message
     this.filterMarkers = function () {
-        var input = this.userInput().toLowerCase();
+        let input = this.userInput().toLowerCase();
         this.hasError(false);
-        var counter = 0;
+        let counter = 0;
 
         this.markerList().forEach(function (marker) {
-            var result = marker.title.toLowerCase().includes(input);
+            let result = marker.title.toLowerCase().includes(input);
             marker.setVisible(result);
             counter += 1;
         });
 
         this.mapObjectList().forEach(function (mapObject) {
-            var result = mapObject.name().toLowerCase().includes(input);
+            let result = mapObject.name().toLowerCase().includes(input);
             mapObject.visible(result);
         });
 
